c61c0bda508555d3bc03d4dede81652e45cbff7c
ankul-shippable <ankul@shippable.com>
2016-09-29 12:47:21 +0530

    scala updates

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 sudo apt-get clean
-sudo mv /var/lib/apt/lists /tmp
+sudo mv /var/lib/apt/lists/* /tmp
 sudo mkdir -p /var/lib/apt/lists/partial
 sudo apt-get clean
 sudo apt-get update
 
 # Install sbt
 echo "================= Install sbt ==================="
-sudo wget https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.8/sbt-launch.jar
+sudo wget https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.12/sbt-launch.jar
 sudo mv sbt-launch.jar /usr/local/bin/sbt-launch.jar
 sudo echo 'SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"' > /usr/local/bin/sbt
 sudo echo 'java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"' >> /usr/local/bin/sbt
---
+++ version/2_10_6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+# Install Scala 2.10.5
+echo "================= Install Scala 2.10.6 ==================="
+sudo wget http://downloads.lightbend.com/scala/2.10.6/scala-2.10.6.tgz
+sudo tar xzf scala-2.10.6.tgz -C /usr/local && rm -f scala-2.10.6.tgz
+echo 'export SCALA_HOME=/usr/local/scala-2.10.6' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$SCALA_HOME/bin' >> $HOME/.bashrc
