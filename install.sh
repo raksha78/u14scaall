75ebeb6f5dc3229dd1576dbaa55942e8a1bc3192
chetantarale <chetan_tarale@yahoo.com>
2017-03-28 14:59:04 +0530

    adds -nv flag to wget command

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ sudo apt-get update
 
 # Install sbt
 echo "================= Install sbt ==================="
-sudo wget https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.12/sbt-launch.jar
-sudo mv sbt-launch.jar /usr/local/bin/sbt-launch.jar
-sudo echo 'SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"' > /usr/local/bin/sbt
-sudo echo 'java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"' >> /usr/local/bin/sbt
-sudo chmod +x /usr/local/bin/sbt
+wget -nv https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.12/sbt-launch.jar
+mv sbt-launch.jar /usr/local/bin/sbt-launch.jar
+echo 'SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"' > /usr/local/bin/sbt
+echo 'java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"' >> /usr/local/bin/sbt
+chmod +x /usr/local/bin/sbt
 printf 'exit\n' | sbt
 
 for file in /u14scaall/version/*;
